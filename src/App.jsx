b6ea62af783193bd728364e55f387606855ccbe0
AlekCawka <awd.ty4ka@gmail.com>
2025-10-03 21:18:05 +0300

    feat(timerUI): добавил отображение таймера в App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,36 @@ import { useState } from 'react';
 import useTimer from './hooks/useTimer';
 
 function App() {
+    const [inputValue, setInputValue] = useState(60);
+    const { seconds, isRunning, startTimer, pauseTimer, resetTimer } = useTimer(inputValue);
 
+    const handleSetTime = () => {
+        resetTimer(inputValue);
+    };
 
     return (
+        <div>
+            <h1>{seconds}</h1>
 
+            <div>
+                <input
+                    type="number"
+                    value={inputValue}
+                    onChange={e => setInputValue(Number(e.target.value))}
+                />
+                <button onClick={handleSetTime}>Установить время</button>
+            </div>
+
+            <div>
+                <button onClick={startTimer} disabled={isRunning || seconds === 0}>
+                    {seconds === inputValue ? 'Старт' : 'Продолжить'}
+                </button>
+                <button onClick={pauseTimer} disabled={!isRunning}>Пауза</button>
+                <button onClick={() => resetTimer()}>Сбросить</button>
+            </div>
+        </div>
     );
 }
 
 export default App;
+
